3a97624ed74a214421d34749b5794cfa97d97338
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2018-06-28 01:11:54 -0500

    added hero-list and crisis-list component and routing links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+
+// configure the application routes.
+// appRoutes is an array of JS objects, with each object containing a route or path in the application.
+// each route maps a URL (path) to a component.
+// appRoutes is passed to the RouterModule.forRoot() to configure the router.
+const appRoutes: Routes = [
+   { path: 'crisis-center', component: CrisisListComponent },
+   // { path: ''}
+   //{ path: 'hero/:id', component: HeroDetailComponent },
+   { path: 'heroes', component: HeroListComponent}
+   //{   // default path
+      //path:'',
+      //redirectTo: '/heroes',
+      //pathMatch: 'full'
+   //},
+   // wildcard route
+   //{ path: '**', component: PageNotFoundComponent } 
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
+   declarations: [
+      AppComponent,
+      HeroListComponent,
+      CrisisListComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule
+      BrowserModule,
+      FormsModule,
+      RouterModule.forRoot(appRoutes, {enableTracing: true})  // enableTracing is added for debugging
   ],
   providers: [],
   bootstrap: [AppComponent]
