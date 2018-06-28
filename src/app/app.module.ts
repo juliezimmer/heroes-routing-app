6807c728298e1c80ee9d5a774bc88c5dc42a336d
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2018-06-28 01:59:30 -0500

    basic router configuration complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 // configure the application routes.
 // appRoutes is an array of JS objects, with each object containing a route or path in the application.
@@ -15,21 +16,23 @@ const appRoutes: Routes = [
    { path: 'crisis-center', component: CrisisListComponent },
    // { path: ''}
    //{ path: 'hero/:id', component: HeroDetailComponent },
-   { path: 'heroes', component: HeroListComponent}
-   //{   // default path
-      //path:'',
-      //redirectTo: '/heroes',
-      //pathMatch: 'full'
-   //},
+   { path: 'heroes', component: HeroListComponent},
+   {   // default path
+      // a redirectTo path requires a pathMatch property
+      path:'',
+      redirectTo: '/heroes',
+      pathMatch: 'full'
+   },
    // wildcard route
-   //{ path: '**', component: PageNotFoundComponent } 
+   { path: '**', component: PageNotFoundComponent } 
 ];
 
 @NgModule({
    declarations: [
       AppComponent,
       HeroListComponent,
-      CrisisListComponent
+      CrisisListComponent,
+      PageNotFoundComponent
   ],
   imports: [
       BrowserModule,
